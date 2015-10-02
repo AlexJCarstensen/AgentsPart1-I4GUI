fc4bc719b35e3f9c92dbd0981ef8404c1c820572
Alex Carstensen <matthacker.alex@gmail.com>
2015-10-02 21:08:50 +0200

    Added Commands in Agents.cs

--- Delopgave4/06-Agents.cs
+++ Delopgave4/06-Agents.cs
@@ -4,17 +4,90 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
 
 namespace Delopgave4
 {
-        public class Agents : ObservableCollection<Agent>
+    public class Agents : ObservableCollection<Agent>, INotifyPropertyChanged
+    {
+        public Agents()
         {
-            public Agents()
+            Add(new Agent("001", "Nina", "Assassination", "UpperVolta"));
+            Add(new Agent("007", "James Bond", "Martinis", "North Korea"));
+            Add(new Agent("005", "Alex","Computer Engineering", "Making it work!"));
+        }
+
+#region Commands
+
+        private ICommand _addCommand;
+        public ICommand AddCommand => _addCommand ?? (_addCommand = new RelayCommand(AddAgent));
+        
+        private void AddAgent()
+        {
+            Add(new Agent());
+            CurrentIndex = Count - 1;
+        }
+
+        private ICommand _deleteCommand;
+        public ICommand DeleteCommand
+         => _deleteCommand ?? (_deleteCommand = new RelayCommand(AgentDelete, AgentDelete_Cane));
+
+        private bool AgentDelete_Cane()
+        {
+            if (Count > 0 && CurrentIndex >= 0)
+                return true;
+            else
+                return false;
+        }
+
+        private void AgentDelete()
+        {
+            RemoveAt(CurrentIndex);
+        }
+
+
+        private ICommand _nextCommand;
+
+        public ICommand NextCommand
+        {
+            get
             {
-                Add(new Agent("001", "Nina", "Assassination", "UpperVolta"));
-                Add(new Agent("007", "James Bond", "Martinis", "North Korea"));
+                return _nextCommand ?? (_nextCommand = new RelayCommand(
+                    () => ++CurrentIndex,
+                    () => CurrentIndex < (Count - 1)));
             }
-        };  // Just to reference it from xaml
-    }
+        }
+
+        private ICommand _prevCommand;
+
+        public ICommand PrevCommand
+            => _prevCommand ?? (_prevCommand = new RelayCommand(PreviousCommandExecute, PreviousCommandCanExecute));
+
+        private bool PreviousCommandCanExecute()
+        {
+            if (CurrentIndex > 0)
+                return true;
+            else
+                return false;
+
+        }
+
+        private void PreviousCommandExecute()
+        {
+            if (CurrentIndex > 0)
+                --CurrentIndex;
+        }
+
+#endregion //Commands
+
+    }  // Just to reference it from xaml
+
+    
+
+ 
+
+}
 
